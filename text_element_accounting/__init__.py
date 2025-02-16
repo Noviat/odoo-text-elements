c2077476f83f297aac65ba953c83a16cf070045b
Jérémy Didderen <didderen.jeremy@gmail.com>
2025-02-16 18:33:21 +0100

    [IMP] rework of all the text elements modules to improve the usability and also add some features like filtering dynamically

--- text_element_accounting/__init__.py
+++ text_element_accounting/__init__.py
@@ -1,1 +1,7 @@
 from . import models
+
+
+def _enable_text_element_on_aml(env):
+    env["ir.model"].search([("model", "=", "account.move")]).write(
+        {"can_be_used_with_text_elements": True}
+    )
