a27532635689e550a93a59ae89c879a30c404ce4
Jérémy Didderen <didderen.jeremy@gmail.com>
2025-02-16 18:33:21 +0100

    [IMP] rework of all the text elements modules to improve the usability and also add some features like filtering dynamically

--- text_element_sale/__init__.py
+++ text_element_sale/__init__.py
@@ -1,1 +1,7 @@
 from . import models
+
+
+def _enable_text_element_on_sale(env):
+    env["ir.model"].search([("model", "=", "sale.order")]).write(
+        {"can_be_used_with_text_elements": True}
+    )
--- text_element_sale/models/sale_order.py
+++ text_element_sale/models/sale_order.py
@@ -1,7 +1,7 @@
 # Copyright 2009-2023 Noviat
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from odoo import api, fields, models
+from odoo import fields, models
 
 
 class SaleOrder(models.Model):
@@ -9,30 +9,8 @@ class SaleOrder(models.Model):
     _inherit = ["sale.order", "text.element.abstract"]
 
     text_element_ids = fields.Many2many(
-        domain=[("res_model", "in", ("sale.order", "all"))]
+        domain=["|", ("model", "=", "sale.order"), ("model", "=", False)]
     )
     text_element_custom_ids = fields.One2many(
-        domain=[("res_model", "in", ("sale.order", "all"))]
+        domain=["|", ("model", "=", "sale.order"), ("model", "=", False)]
     )
-
-    @api.depends("partner_id")
-    def _compute_report_lang(self):
-        for rec in self:
-            if rec.partner_id.lang:
-                rec.report_lang = rec.partner_id.lang
-            else:
-                rec.report_lang = self.env.lang
-
-    def _get_text_element_records(self, position):
-        self.ensure_one()
-        text_elements, custom_text_elements = super()._get_text_element_records(
-            position
-        )
-        if not self.env.context.get("proforma"):
-            text_elements = text_elements.filtered(
-                lambda te: not te.only_shown_in_proforma
-            )
-            custom_text_elements = custom_text_elements.filtered(
-                lambda te: not te.only_shown_in_proforma
-            )
-        return text_elements, custom_text_elements
