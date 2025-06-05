0f21c08b417d53a0f4fbd1f4b9a35a03aecffa18
Jérémy Didderen <didderen.jeremy@gmail.com>
2025-06-05 14:10:21 +0200

    [ADD] text_element_base / text_element_accounting / text_element_sale

---
+++ text_element_accounting/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2009-2023 Noviat
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Text Elements - Accounting module",
+    "version": "18.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Noviat, Elneo",
+    "website": "https://www.noviat.com/",
+    "category": "Accounting",
+    "summary": "Text Elements - Accounting module",
+    "depends": ["text_element_base", "account"],
+    "data": [
+        "views/account_move_views.xml",
+        "views/invoice_report.xml",
+        "views/text_element_views.xml",
+    ],
+    "installable": True,
+}
