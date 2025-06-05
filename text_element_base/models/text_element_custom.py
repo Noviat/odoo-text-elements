5ccd76d2467b0df31d9de30b363048cc7ced49ba
Jérémy Didderen <didderen.jeremy@gmail.com>
2025-06-05 14:10:21 +0200

    [ADD] text_element_base / text_element_accounting / text_element_sale

---
+++ text_element_base/models/text_element_custom.py
@@ -0,0 +1,20 @@
+# Copyright 2009-2023 Noviat
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from odoo import fields, models
+
+
+class TextElementCustom(models.Model):
+    _name = "text.element.custom"
+    _inherit = [
+        "text.element",
+    ]
+    _description = "Custom text elements"
+    _order = "sequence, id"
+    _check_company_auto = True
+
+    res_id = fields.Integer(required=True, string="Record ID")
+    res_model = fields.Selection(required=True)
+    text_element_id = fields.Many2one(
+        comodel_name="text.element", string="Text Element"
+    )
