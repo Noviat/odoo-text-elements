62880a6a854c48f96f8358f21f83b33a4873cca2
Jérémy Didderen <didderen.jeremy@gmail.com>
2025-07-14 10:27:50 +0200

    [FIX] replace all tree with list

--- text_element_accounting/__manifest__.py
+++ text_element_accounting/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Text Elements - Accounting module",
-    "version": "18.0.2.0.0",
+    "version": "18.0.2.1.0",
     "license": "AGPL-3",
     "author": "Noviat, Elneo",
     "website": "https://www.noviat.com/",
